16c0f1eab8e0a71963726b941ca7c63246de7a78
Robert Geil <robert@rgeil.net>
2019-01-22 11:56:47 -0800

    Update resume, fix nav bar jumping on affix

--- public/js/main.js
+++ public/js/main.js
@@ -1,9 +1,7 @@
 
 function main() {
 
-(function () {
-   'use strict';
-   
+(function () {   
   	$('a.page-scroll').click(function() {
         if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
           var target = $(this.hash);
@@ -23,43 +21,6 @@ $('#nav').affix({
         top: $('header').height()
       }
 });	
-
-	
-  	// Portfolio isotope filter
-    $(window).load(function() {
-        var $container = $('.portfolio-items');
-        $container.isotope({
-            filter: '*',
-            animationOptions: {
-                duration: 750,
-                easing: 'linear',
-                queue: false
-            }
-        });
-        $('.cat a').click(function() {
-            $('.cat .active').removeClass('active');
-            $(this).addClass('active');
-            var selector = $(this).attr('data-filter');
-            $container.isotope({
-                filter: selector,
-                animationOptions: {
-                    duration: 750,
-                    easing: 'linear',
-                    queue: false
-                }
-            });
-            return false;
-        });
-
-    });
-	
-
-    // Nivo Lightbox 
-    $('.portfolio-item a').nivoLightbox({
-            effect: 'slideDown',  
-            keyboardNav: true,                            
-        });
- 
 
 }());
 
