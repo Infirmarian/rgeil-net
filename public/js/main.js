51e5b43ec9322d50d12ec54a1f649836ff18ec23
Robert Geil <robert@rgeil.net>
2019-12-07 13:12:39 -0800

    Fully implemented dark mode! and added lots of SVG images to reduce transfer sizes

--- public/js/main.js
+++ public/js/main.js
@@ -1,32 +1,48 @@
-
-function main() {
-
-(function () {   
-  	$('a.page-scroll').click(function() {
-        if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-          var target = $(this.hash);
-          target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-          if (target.length) {
-            $('html,body').animate({
+const main = () => {
+  (function() {
+    $("a.page-scroll").click(function() {
+      if (
+        location.pathname.replace(/^\//, "") ==
+          this.pathname.replace(/^\//, "") &&
+        location.hostname == this.hostname
+      ) {
+        var target = $(this.hash);
+        target = target.length
+          ? target
+          : $("[name=" + this.hash.slice(1) + "]");
+        if (target.length) {
+          $("html,body").animate(
+            {
               scrollTop: target.offset().top - 40
-            }, 900);
-            return false;
-          }
+            },
+            900
+          );
+          return false;
         }
-      });
-
-	// affix the navbar after scroll below header
-$('#nav').affix({
-      offset: {
-        top: $('header').height()
       }
     });
-$('#nav-wrapper').css({
-	'min-height': $('#nav').height() + 'px'
-});
 
-}());
-
-
+    // affix the navbar after scroll below header
+    $("#nav").affix({
+      offset: {
+        top: $("header").height()
+      }
+    });
+    $("#nav-wrapper").css({
+      "min-height": $("#nav").height() + "px"
+    });
+  })();
+};
+if (
+  window.matchMedia &&
+  window.matchMedia("(prefers-color-scheme: dark)").matches
+) {
+  console.log("DARK MODE!!");
+  const elements = document.getElementsByClassName("dark-convertable-image");
+  for (let i = 0; i < elements.length; i++) {
+    const arr = elements[i].src.split("/");
+    arr[arr.length - 1] = "dark." + arr[arr.length - 1];
+    elements[i].src = arr.join("/");
+  }
 }
 main();
